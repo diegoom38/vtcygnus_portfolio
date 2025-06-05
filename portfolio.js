619cdb4d9fc373f93c65b4a456f2afbe807abace
Diego Oliveira Maciel <50427109+diegoom38@users.noreply.github.com>
2025-06-04 22:23:30 -0300

    feature: menu fixed

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,26 @@
 function downloadProject(name) {
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = name; // caminho do arquivo (ex: 'gato-preto.pdf')
   link.download = name; // força o download
-  link.style.display = 'none';
-  
+  link.style.display = "none";
+
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 }
+
+// Menu hamburger functionality
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+// Fechar o menu quando um link for clicado (opcional)
+const menuLinks = document.querySelectorAll("#mobile-menu a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+});
